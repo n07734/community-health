808cdba9a65cd332a09a9202604d2daa82c68e66
n07734 <chris@chriss-MacBook-Pro.local>
2021-01-09 20:49:31 +0000

    tweek grid rows

--- src/components/home/DataOptions.js
+++ src/components/home/DataOptions.js
@@ -260,14 +260,11 @@ const styles = theme => ({
     form: {
         display: 'grid',
         gridTemplateColumns: 'repeat(3, 1fr)',
-        gridTemplateRows: '1fr auto 1fr 1fr',
         marginBottom: '1rem',
         columnGap: '8px',
         rowGap: '8px', // BUG: theme.spacing.unit does not have px for row but does for column, odd
         '@media (max-width: 768px)': {
             gridTemplateColumns: 'repeat(1, 1fr)',
-            gridTemplateRows: 'auto',
-
         },
         '& button': {
             minHeight: '3rem',
