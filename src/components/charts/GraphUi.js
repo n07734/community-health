d08d53900f1cbcfae2f9159c7993d61e6104a512
n07734 <n07734@gmail.com>
2022-09-28 20:51:42 +0100

    split out graphui

---
+++ src/components/charts/GraphUi.js
@@ -0,0 +1,334 @@
+import React, { useState } from 'react'
+import { withStyles } from '@material-ui/core/styles'
+import {
+    Select,
+    MenuItem,
+    RadioGroup,
+    Radio,
+    FormLabel,
+} from '@material-ui/core'
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+
+import colors from '../colors'
+import { P } from '../shared/StyledTags'
+import Button from '../shared/Button'
+
+const lineOptions = [
+    {
+        label: 'All comments',
+        dataKey: 'comments',
+    },
+    {
+        label: 'Code comments',
+        dataKey: 'codeComments',
+    },
+    {
+        label: 'Review comments',
+        dataKey: 'generalComments',
+    },
+    {
+        label: 'Approvals',
+        dataKey: 'approvals',
+    },
+    {
+        label: 'Additions',
+        dataKey: 'additions'
+    },
+    {
+        label: 'Deletions',
+        dataKey: 'deletions'
+    },
+    {
+        label: 'PR size',
+        dataKey: 'prSize',
+    },
+    {
+        label: 'Age(days)',
+        dataKey: 'age',
+    },
+    {
+        label: 'Sentiment',
+        dataKey: 'commentSentimentTotalScore',
+    },
+    {
+        label: 'Sentiment to team',
+        dataKey: 'commentSentimentScore',
+    },
+    {
+        label: 'Sentiment from team',
+        dataKey: 'commentAuthorSentimentScore',
+    },
+]
+
+const mathWords = {
+    average: 'Average',
+    sum: 'Total',
+}
+
+const addedLine = (removeLine, classes) => ({
+    color = '',
+    label = '',
+    dataKey = '',
+    groupMath = 'average',
+} = {}, i) => <div key={i}>
+        <P className={`${classes.savedLine} ${classes[color]}`}>{mathWords[groupMath]} {label} </P>
+        <RemoveCircleIcon
+            className={`${classes.remove} ${classes[color]}`}
+            onClick={event => {
+                event.preventDefault()
+                removeLine(dataKey)
+            }}
+        />
+    </div>
+
+const getRemainingLines = (graphInfo = {}) => {
+    const activeLines = [
+        ...(graphInfo.left || []),
+        ...(graphInfo.right || []),
+    ]
+
+    const remainingLines = lineOptions
+        .filter(({ dataKey = '' } = {}) => activeLines.every(x => x.dataKey !== dataKey))
+
+    return remainingLines
+}
+
+const GraphUi = ({
+    graphInfo = {},
+    setGraph = () => {},
+    graphs = [],
+    classes = {},
+}) =>  {
+    const remainingLines = getRemainingLines(graphInfo)
+
+    const [formInfo, setFormInfo] = useState({
+        label: remainingLines[0]?.label || 'Comments',
+        dataKey: remainingLines[0]?.dataKey || 'comments',
+        color: colors[1],
+        lineSide: 'left',
+        groupMath: 'average',
+    })
+
+    const setValue = (newValue = {}) => {
+        setFormInfo({
+            ...formInfo,
+            ...newValue,
+        })
+    }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        const side = formInfo.lineSide
+        const lines = graphInfo[side] || []
+        lines.push(formInfo)
+
+        const graphItem = {
+            ...graphInfo,
+            [side]: lines,
+        }
+
+        const updatedGraphs = graphs
+            .map(x => graphItem.graphId === x.graphId
+                ? graphItem
+                : x
+            )
+
+        setGraph(updatedGraphs)
+
+        const [firstline = {}] = getRemainingLines(graphItem)
+
+        const chosenColorIndex = colors
+            .findIndex(x => x === formInfo.color)
+
+        setFormInfo({
+            label: firstline.label,
+            dataKey: firstline.dataKey,
+            color: colors[(chosenColorIndex + 1) % colors.length],
+            lineSide: 'left',
+            groupMath: 'average',
+        })
+    }
+
+    const removeLine = (side) => (dataKey) => {
+        const lines = graphInfo[side] || []
+        const updatedLines = lines
+            .filter(x => x.dataKey !== dataKey)
+
+        const removedLine = lines
+            .find(x => x.dataKey === dataKey)
+
+        const graphItem = {
+            ...graphInfo,
+            [side]: updatedLines,
+        }
+
+        const updatedGraphs = graphs
+            .map(x => graphItem.graphId === x.graphId
+                ? graphItem
+                : x
+            )
+        setGraph(updatedGraphs)
+
+        const [firstline = {}] = getRemainingLines(graphItem)
+        setFormInfo({
+            label: firstline.label,
+            dataKey: firstline.dataKey,
+            color: removedLine.color,
+            lineSide: 'left',
+            groupMath: 'average',
+        })
+    }
+
+    return <div className={classes.graphForm}>
+        {
+            remainingLines.length > 0 && <form className={classes.graphLine} onSubmit={handleSubmit}>
+                <Select
+                    onChange={(e) => {
+                        const value = e.target.value
+                        const { label } = remainingLines.find(x => x.dataKey === value)
+                        setValue({
+                            dataKey: value,
+                            label
+                        })
+
+                    }}
+                    value={formInfo.dataKey}
+                    inputProps={{ 'aria-label': 'choose a line' }}
+                >
+                    {
+                        remainingLines
+                            .map(({label, dataKey} = {}) => <MenuItem key={dataKey} value={dataKey} >
+                                {label}
+                            </MenuItem>)
+                    }
+                </Select>
+                <Select
+                    onChange={(e) => setValue({ color: e.target.value })}
+                    value={formInfo.color}
+                    inputProps={{ 'aria-label': 'Choose a color' }}
+                >
+                    {
+                        colors
+                            .map((color, i) => <MenuItem key={color} value={color} >
+                                <div style={{
+                                    backgroundColor: color,
+                                    width: '70px',
+                                    height: '10px',
+                                    marginTop: '5px',
+                                }}></div>
+                            </MenuItem>)
+                    }
+                </Select>
+                <RadioGroup
+                    value={formInfo.lineSide}
+                    onChange={(e) => setValue({ lineSide: e.target.value })}
+                    row name="side"
+                >
+                    <FormLabel>Axis: Left<Radio name="side" value="left" /></FormLabel>
+                    <FormLabel>Right<Radio name="side" value="right" /></FormLabel>
+                </RadioGroup>
+                <RadioGroup
+                    value={formInfo.groupMath}
+                    onChange={(e) => setValue({ groupMath: e.target.value })}
+                    row
+                    name="lineMaths"
+                >
+                    <FormLabel>Line points: Averaged<Radio name="lineMaths" value="average" /></FormLabel>
+                    <FormLabel>Totaled<Radio name="lineMaths" value="sum" /></FormLabel>
+                </RadioGroup>
+                <Button value={"Add to graph"} color="primary" type='submit'/>
+            </form>
+        }
+        <div className={classes.customLines}>
+            <div>
+            {
+                (graphInfo.left || [])
+                    .map(addedLine(removeLine('left'), classes))
+            }
+            </div>
+            <div>
+            {
+                (graphInfo.right || [])
+                    .map(addedLine(removeLine('right'), classes))
+            }
+            </div>
+        </div>
+    </div>
+}
+
+const colorClasses = {}
+colors
+    .forEach((color) => {
+        colorClasses[color] = {color: color, borderColor: color}
+    })
+
+const styles = theme => ({
+    graphLine: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        borderRadius: '15px',
+        paddingTop:' 0.5em',
+        justifyContent: 'center',
+        backgroundImage: 'linear-gradient(0deg, rgba(102,102,102,0) 80%, rgba(102,102,102,1) 100%)',
+        '& p': {
+            margin: '0'
+        },
+        '& > *': {
+            marginRight: '20px',
+            marginBottom: '15px',
+        },
+        '& .MuiFormLabel-root': {
+            color: theme.palette.mainCopy.color,
+        },
+    },
+    graphForm: {
+        width: '100%',
+        maxWidth: '1200px',
+    },
+    remove: {
+        fontSize: '22px',
+        marginLeft: '5px',
+        marginBottom: '-5px',
+    },
+    savedLine: {
+        fontSize: '1.3em',
+        borderBottom: `solid 2px`,
+        display: 'inline-block',
+        lineHeight: '2rem',
+        position: 'relative',
+        '&:before': {
+            lineHeight: '0',
+            content: '"•"',
+            position: 'absolute',
+            bottom: '-1px',
+            left: '-3px',
+        },
+        '&:after': {
+            lineHeight: '0',
+            content: '"•"',
+            position: 'absolute',
+            bottom: '-1px',
+            right: '-3px',
+        },
+    },
+    customLines: {
+        width: '100%',
+        display: 'flex',
+        flexWrap: 'nowrap',
+        justifyContent: 'space-between',
+        '& > div': {
+            columnGap: '20px',
+            display: 'flex',
+            flexWrap: 'wrap',
+            maxWidth: '50%',
+            justifyContent: 'flex-start',
+        },
+        '& > div:nth-child(2)': {
+            justifyContent: 'flex-end',
+            textAlign: 'right',
+        },
+    },
+    ...colorClasses,
+})
+
+export default withStyles(styles)(GraphUi)
--- src/components/sections/PullRequestCustom.js
+++ src/components/sections/PullRequestCustom.js
@@ -1,70 +1,15 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import { withStyles } from '@material-ui/core/styles'
-import {
-    Select,
-    MenuItem,
-    RadioGroup,
-    Radio,
-    FormLabel,
-} from '@material-ui/core'
-import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
 
 import Paper from '../shared/Paper'
 import ChartDescription from '../shared/ChartDescription'
 import { P } from '../shared/StyledTags'
 import Button from '../shared/Button'
-import colors from '../colors'
 import Line from '../charts/Line'
+import GraphUi from '../charts/GraphUi'
 import ItemsTable from './ItemsTable'
 import { chunkData } from '../charts/lineHelpers'
-
-const lineOptions = [
-    {
-        label: 'All comments',
-        dataKey: 'comments',
-    },
-    {
-        label: 'Code comments',
-        dataKey: 'codeComments',
-    },
-    {
-        label: 'Review comments',
-        dataKey: 'generalComments',
-    },
-    {
-        label: 'Approvals',
-        dataKey: 'approvals',
-    },
-    {
-        label: 'Additions',
-        dataKey: 'additions'
-    },
-    {
-        label: 'Deletions',
-        dataKey: 'deletions'
-    },
-    {
-        label: 'PR size',
-        dataKey: 'prSize',
-    },
-    {
-        label: 'Age(days)',
-        dataKey: 'age',
-    },
-    {
-        label: 'Sentiment',
-        dataKey: 'commentSentimentTotalScore',
-    },
-    {
-        label: 'Sentiment to team',
-        dataKey: 'commentSentimentScore',
-    },
-    {
-        label: 'Sentiment from team',
-        dataKey: 'commentAuthorSentimentScore',
-    },
-]
 
 const formatGraphData = (pullRequests = []) => (data = {}) => {
     const {
@@ -100,24 +45,6 @@ const formatGraphData = (pullRequests = []) => (data = {}) => {
     return formData
 }
 
-const mathWords = {
-    average: 'Average',
-    sum: 'Total',
-}
-
-const getColor = (formInfo = {}) => {
-    const usedColors = [
-        ...(formInfo.left || []),
-        ...(formInfo.right || []),
-    ]
-        .map(x => x.color)
-
-    const [ color ] = colors
-        .filter(x => !usedColors.includes(x))
-
-    return color
-}
-
 const getTableKeys = (graphInfo = {}) => {
     const activeKeys = [
         ...(graphInfo.left || []),
@@ -130,191 +57,6 @@ const getTableKeys = (graphInfo = {}) => {
         : ['comments', 'approvals', 'age', 'prSize', 'author']
 
     return keys
-}
-
-const addedLine = (removeLine, classes) => ({
-    color = '',
-    label = '',
-    dataKey = '',
-    groupMath = 'average',
-} = {}, i) => <div key={i}>
-        <P className={`${classes.savedLine} ${classes[color]}`}>{mathWords[groupMath]} {label} </P>
-        <RemoveCircleIcon
-            className={`${classes.remove} ${classes[color]}`}
-            onClick={event => {
-                event.preventDefault()
-                removeLine(dataKey)
-            }}
-        />
-    </div>
-
-const getRemainingLines = (graphInfo = {}) => {
-    const activeLines = [
-        ...(graphInfo.left || []),
-        ...(graphInfo.right || []),
-    ]
-
-    const remainingLines = lineOptions
-        .filter(({ dataKey = '' } = {}) => activeLines.every(x => x.dataKey !== dataKey))
-
-    return remainingLines
-}
-
-const GraphUi = ({
-    graphInfo = {},
-    setGraph = () => {},
-    graphs = [],
-    classes = {},
-}) =>  {
-    const remainingLines = getRemainingLines(graphInfo)
-
-    const [formInfo, setFormInfo] = useState({
-        label: remainingLines[0]?.label || 'Comments',
-        dataKey: remainingLines[0]?.dataKey || 'comments',
-        color: getColor(graphInfo),
-        lineSide: 'left',
-        groupMath: 'average',
-    })
-
-    const setValue = (newValue = {}) => {
-        setFormInfo({
-            ...formInfo,
-            ...newValue,
-        })
-    }
-
-    const handleSubmit = (event) => {
-        event.preventDefault()
-        const side = formInfo.lineSide
-        const lines = graphInfo[side] || []
-        lines.push(formInfo)
-
-        const graphItem = {
-            ...graphInfo,
-            [side]: lines,
-        }
-
-        const updatedGraphs = graphs
-            .map(x => graphItem.graphId === x.graphId
-                ? graphItem
-                : x
-            )
-
-        setGraph(updatedGraphs)
-
-        const [firstline = {}] = getRemainingLines(graphItem)
-
-        setFormInfo({
-            label: firstline.label,
-            dataKey: firstline.dataKey,
-            color: getColor(graphItem),
-            lineSide: 'left',
-            groupMath: 'average',
-        })
-    }
-
-    const removeLine = (side) => (dataKey) => {
-        const lines = graphInfo[side] || []
-        const updatedLines = lines
-            .filter(x => x.dataKey !== dataKey)
-
-        const graphItem = {
-            ...graphInfo,
-            [side]: updatedLines,
-        }
-
-        const updatedGraphs = graphs
-            .map(x => graphItem.graphId === x.graphId
-                ? graphItem
-                : x
-            )
-        setGraph(updatedGraphs)
-
-        const [firstline = {}] = getRemainingLines(graphItem)
-        setFormInfo({
-            label: firstline.label,
-            dataKey: firstline.dataKey,
-            color: getColor(graphItem),
-            lineSide: 'left',
-            groupMath: 'average',
-        })
-    }
-
-    return <div className={classes.graphForm}>
-        {
-            remainingLines.length > 0 && <form className={classes.graphLine} onSubmit={handleSubmit}>
-                <Select
-                    onChange={(e) => {
-                        const value = e.target.value
-                        const { label } = remainingLines.find(x => x.dataKey === value)
-                        setValue({
-                            dataKey: value,
-                            label
-                        })
-
-                    }}
-                    value={formInfo.dataKey}
-                    inputProps={{ 'aria-label': 'choose a line' }}
-                >
-                    {
-                        remainingLines
-                            .map(({label, dataKey} = {}) => <MenuItem key={dataKey} value={dataKey} >
-                                {label}
-                            </MenuItem>)
-                    }
-                </Select>
-                <Select
-                    onChange={(e) => setValue({ color: e.target.value })}
-                    value={formInfo.color}
-                    inputProps={{ 'aria-label': 'Choose a color' }}
-                >
-                    {
-                        colors
-                            .map((color, i) => <MenuItem key={color} value={color} >
-                                <div style={{
-                                    backgroundColor: color,
-                                    width: '70px',
-                                    height: '10px',
-                                    marginTop: '5px',
-                                }}></div>
-                            </MenuItem>)
-                    }
-                </Select>
-                <RadioGroup
-                    value={formInfo.lineSide}
-                    onChange={(e) => setValue({ lineSide: e.target.value })}
-                    row name="side"
-                >
-                    <FormLabel>Axis: Left<Radio name="side" value="left" /></FormLabel>
-                    <FormLabel>Right<Radio name="side" value="right" /></FormLabel>
-                </RadioGroup>
-                <RadioGroup
-                    value={formInfo.groupMath}
-                    onChange={(e) => setValue({ groupMath: e.target.value })}
-                    row
-                    name="lineMaths"
-                >
-                    <FormLabel>Line points: Averaged<Radio name="lineMaths" value="average" /></FormLabel>
-                    <FormLabel>Totaled<Radio name="lineMaths" value="sum" /></FormLabel>
-                </RadioGroup>
-                <Button value={"Add to graph"} color="primary" type='submit'/>
-            </form>
-        }
-        <div className={classes.customLines}>
-            <div>
-            {
-                (graphInfo.left || [])
-                    .map(addedLine(removeLine('left'), classes))
-            }
-            </div>
-            <div>
-            {
-                (graphInfo.right || [])
-                    .map(addedLine(removeLine('right'), classes))
-            }
-            </div>
-        </div>
-    </div>
 }
 
 let id = 1
@@ -330,23 +72,11 @@ const PullRequestCustom = ({
         left: [
             {
                 label: 'Comments',
-                color: '#1F77B4',
+                color: '#E82573',
                 dataKey: 'comments',
             },
-            // {
-            //     label: 'Approvals',
-            //     color: '#E82573',
-            //     dataKey: 'approvals',
-            // },
         ],
-        right: [
-            // {
-            //     label: 'PR Size',
-            //     color: '#4ECC7A',
-            //     groupMath: 'sum',
-            //     dataKey: 'prSize',
-            // }
-        ]
+        right: []
     }]
     const [graphs, setGraph] = useState(defaultState)
 
@@ -378,7 +108,6 @@ const PullRequestCustom = ({
                             graphInfo={graphInfo}
                             setGraph={setGraph}
                             graphs={graphs}
-                            classes={classes}
                         />
                         <Line
                             blockHeading={true}
@@ -393,32 +122,34 @@ const PullRequestCustom = ({
                         }
                     </div>)
             }
-            {
-                graphs.length > 1
-                    && <Button
-                        value="Remove above graph"
-                        color="secondary"
-                        onClick={(event) => {
-                            event.preventDefault()
-                            setGraph(graphs.slice(0, -1))
-                        }}
-                    />
-            }
-            <Button
-                value={"Make another graph"}
-                color="primary"
-                onClick={(event) => {
-                    event.preventDefault()
-                    setGraph([
-                        ...graphs,
-                        {
-                            graphId: getGraphId(),
-                            left: [],
-                            right: [],
-                        },
-                    ])
-                }}
-            />
+            <div className={classes.buttons}>
+                {
+                    graphs.length > 1
+                        && <Button
+                            value="Remove above graph"
+                            color="secondary"
+                            onClick={(event) => {
+                                event.preventDefault()
+                                setGraph(graphs.slice(0, -1))
+                            }}
+                        />
+                }
+                <Button
+                    value={"Make another graph"}
+                    color="primary"
+                    onClick={(event) => {
+                        event.preventDefault()
+                        setGraph([
+                            ...graphs,
+                            {
+                                graphId: getGraphId(),
+                                left: [],
+                                right: [],
+                            },
+                        ])
+                    }}
+                />
+            </div>
         </Paper>
     )
 }
@@ -428,40 +159,7 @@ const mapStateToProps = (state) => ({
     releases: state.releases,
 })
 
-const colorClasses = {}
-colors
-    .forEach((color) => {
-        colorClasses[color] = {color: color, borderColor: color}
-    })
-
 const styles = theme => ({
-    graphLine: {
-        display: 'flex',
-        flexWrap: 'wrap',
-        borderRadius: '15px',
-        paddingTop:' 0.5em',
-        justifyContent: 'center',
-        backgroundImage: 'linear-gradient(0deg, rgba(102,102,102,0) 80%, rgba(102,102,102,1) 100%)',
-        '& p': {
-            margin: '0'
-        },
-        '& > *': {
-            marginRight: '20px',
-            marginBottom: '15px',
-        },
-        '& .MuiFormLabel-root': {
-            color: theme.palette.mainCopy.color,
-        },
-    },
-    graphForm: {
-        width: '100%',
-        maxWidth: '1200px',
-    },
-    remove: {
-        fontSize: '22px',
-        marginLeft: '5px',
-        marginBottom: '-5px',
-    },
     addedGraph: {
         backgroundRepeat: 'no-repeat',
         backgroundPosition: 'top left',
@@ -472,51 +170,14 @@ const styles = theme => ({
         flexWrap: 'wrap',
         justifyContent: 'center',
     },
-    fill: {
-        flexBasis: '100%',
-    },
-    savedLine: {
-        fontSize: '1.3em',
-        borderBottom: `solid 2px`,
-        display: 'inline-block',
-        lineHeight: '2rem',
-        position: 'relative',
-        '&:before': {
-            lineHeight: '0',
-            content: '"•"',
-            position: 'absolute',
-            bottom: '-1px',
-            left: '-3px',
-        },
-        '&:after': {
-            lineHeight: '0',
-            content: '"•"',
-            position: 'absolute',
-            bottom: '-1px',
-            right: '-3px',
-        },
-    },
-    customLines: {
-        width: '100%',
+    buttons: {
         display: 'flex',
         flexWrap: 'nowrap',
-        justifyContent: 'space-between',
-        '& > div': {
-            columnGap: '20px',
-            display: 'flex',
-            flexWrap: 'wrap',
-            maxWidth: '50%',
-            justifyContent: 'flex-start',
-        },
-        '& > div:nth-child(2)': {
-            justifyContent: 'flex-end',
-            textAlign: 'right',
-        },
+        justifyContent: 'center',
     },
     block: {
         display: 'block',
     },
-    ...colorClasses,
 })
 
 export default connect(mapStateToProps)(withStyles(styles)(PullRequestCustom))
